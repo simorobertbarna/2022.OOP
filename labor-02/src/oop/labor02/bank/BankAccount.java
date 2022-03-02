71582e5d32ef05b1170d7ffae968d525958af825
Simó Róbert-Barna <simo.robert.barna@student.ms.sapientia.ro>
2022-03-02 21:26:37 +0200

    OOP

---
+++ labor-02/src/oop/labor02/bank/BankAccount.java
@@ -0,0 +1,41 @@
+package oop.labor02.bank;
+
+public class BankAccount {
+
+    //Attributes
+    private String accountNumber;
+    private double balance;
+
+    //Constructor
+    public BankAccount(String accountNumber){
+        this.accountNumber = accountNumber;
+        this.balance = 0;
+    }
+
+    public String getAccountNumber(){
+        return accountNumber;
+    }
+
+    public double getBalance(){
+        return balance;
+    }
+
+    public void deposit(double sum){
+        if( sum > 0) {
+            this.balance = (int)((double)this.balance + sum);
+        }
+    }
+    public boolean withdraw(double money){
+        boolean result = false;
+
+        if(this.balance >= money){
+            this.balance -= money;
+            result = true;
+        }
+        return false;
+    }
+
+    public String toString(){
+        return "Account: " + this.accountNumber + ", balance: " + this.balance;
+    }
+}
---
+++ labor-02/src/oop/labor02/date/MyDate.java
@@ -0,0 +1,36 @@
+package oop.labor02.date;
+
+public class MyDate {
+        private int day;
+        private int month;
+        private int year;
+
+    public MyDate(int a,int b,int c){
+        if(DateUtil.isValidDate(a,b,c)){
+            this.year=a;
+            this.month=b;
+            this.day=c;
+        }else{
+            System.out.println("Invalid date!");
+        }
+
+    }
+
+    public int getYear(){
+        return year;
+    }
+
+    public int getMonth(){
+        return month;
+    }
+
+    public int getDay(){
+        return day;
+    }
+
+    @Override
+    public String toString(){
+        return "Date: " + this.year + "," + this.month + "," + this.day;
+    }
+
+}
